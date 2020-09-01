ac75ac719dc95105fc2c8d51c7de5bfa062a8749
Mavr1 <mavrodiy.m.e@gmail.com>
2020-09-02 00:25:27 +0300

    minor fixes

--- contacts.js
+++ contacts.js
@@ -9,40 +9,38 @@ module.exports.listContacts = async function () {
 };
 
 module.exports.getContactById = async function (contactId) {
-  const response = await fs.promises.readFile(contactsPath, 'utf8');
-  const contacts = JSON.parse(response);
-  const foundContact = contacts.find((item) => item.id === contactId);
-  console.log('foundContact :>> ', foundContact);
-};
-
-module.exports.removeContact = async function (contactId) {
-  const response = await fs.promises.readFile(contactsPath, 'utf8');
-  const contacts = JSON.parse(response);
-  const filteredContacts = contacts.filter((item) => item.id !== contactId);
-  await fs.promises.writeFile(
-    contactsPath,
-    JSON.stringify(filteredContacts),
-    function (err) {
-      if (err) {
-        console.log('err :>> ', err);
-        throw err;
-      }
-    }
-  );
-  console.log('Succesfully deleted:', contactId);
-};
-
-module.exports.addContact = async function (name, email, phone) {
-  const response = await fs.promises.readFile(contactsPath, 'utf8');
-  const contacts = JSON.parse(response);
-  const id = Math.max(...contacts.map((item) => item.id)) + 1;
-  const newContact = { id, name, email, phone };
-  contacts.push(newContact);
   try {
-    await fs.promises.writeFile(contactsPath, JSON.stringify(contacts));
+    const response = await fs.promises.readFile(contactsPath, 'utf8');
+    const contacts = JSON.parse(response);
+    const foundContact = contacts.find((item) => item.id === contactId);
+    console.log('foundContact :>> ', foundContact);
   } catch (error) {
     console.log('error :>> ', error);
   }
+};
 
-  console.log('Succesfully saved:', newContact);
+module.exports.removeContact = async function (contactId) {
+  try {
+    const response = await fs.promises.readFile(contactsPath, 'utf8');
+    const contacts = JSON.parse(response);
+    const filteredContacts = contacts.filter((item) => item.id !== contactId);
+    await fs.promises.writeFile(contactsPath, JSON.stringify(filteredContacts));
+    console.log('Succesfully deleted:', contactId);
+  } catch (error) {
+    console.log('error :>> ', error);
+  }
+};
+
+module.exports.addContact = async function (name, email, phone) {
+  try {
+    const response = await fs.promises.readFile(contactsPath, 'utf8');
+    const contacts = JSON.parse(response);
+    const id = Math.max(...contacts.map((item) => item.id)) + 1;
+    const newContact = { id, name, email, phone };
+    contacts.push(newContact);
+    await fs.promises.writeFile(contactsPath, JSON.stringify(contacts));
+    console.log('Succesfully saved:', newContact);
+  } catch (error) {
+    console.log('error :>> ', error);
+  }
 };
