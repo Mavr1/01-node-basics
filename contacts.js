baf247fb93e2b0e4f2972e7c9b20ffc1b45aed4a
Mavr1 <mavrodiy.m.e@gmail.com>
2020-09-01 18:33:47 +0300

    fixed according to review

--- contacts.js
+++ contacts.js
@@ -2,18 +2,22 @@ const fs = require('fs');
 const path = require('path');
 
 const contactsPath = path.join(__dirname, './db/contacts.json');
-const contacts = require(contactsPath);
 
-module.exports.listContacts = function () {
-  console.log('contacts :>> ', contacts);
+module.exports.listContacts = async function () {
+  const contacts = await fs.promises.readFile(contactsPath, 'utf8');
+  console.log('contacts :>> ', JSON.parse(contacts));
 };
 
-module.exports.getContactById = function (contactId) {
+module.exports.getContactById = async function (contactId) {
+  const response = await fs.promises.readFile(contactsPath, 'utf8');
+  const contacts = JSON.parse(response);
   const foundContact = contacts.find((item) => item.id === contactId);
   console.log('foundContact :>> ', foundContact);
 };
 
 module.exports.removeContact = async function (contactId) {
+  const response = await fs.promises.readFile(contactsPath, 'utf8');
+  const contacts = JSON.parse(response);
   const filteredContacts = contacts.filter((item) => item.id !== contactId);
   await fs.promises.writeFile(
     contactsPath,
@@ -29,16 +33,16 @@ module.exports.removeContact = async function (contactId) {
 };
 
 module.exports.addContact = async function (name, email, phone) {
+  const response = await fs.promises.readFile(contactsPath, 'utf8');
+  const contacts = JSON.parse(response);
   const id = Math.max(...contacts.map((item) => item.id)) + 1;
   const newContact = { id, name, email, phone };
   contacts.push(newContact);
-  await fs.promises.writeFile(contactsPath, JSON.stringify(contacts), function (
-    err
-  ) {
-    if (err) {
-      console.log('err :>> ', err);
-      throw err;
-    }
-  });
+  try {
+    await fs.promises.writeFile(contactsPath, JSON.stringify(contacts));
+  } catch (error) {
+    console.log('error :>> ', error);
+  }
+
   console.log('Succesfully saved:', newContact);
 };
